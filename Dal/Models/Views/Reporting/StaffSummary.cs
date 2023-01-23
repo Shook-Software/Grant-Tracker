adbe4c95db1d4233134b3639b3c6bfbb9f91d58b
PapayaPuppet <ethan.p.shook.us@gmail.com>
2023-01-23 13:13:46 -0500

    Attendance editing fix, major revamp to reporting in front and back ends:

---
+++ Dal/Models/Views/Reporting/StaffSummary.cs
@@ -0,0 +1,24 @@
+﻿namespace GrantTracker.Dal.Models.Views.Reporting;
+
+public class StaffSummaryDbModel
+{
+	public string OrganizationName { get; set; }
+	public Guid InstructorSchoolYearGuid { get; set; }
+	public string Status { get; set; }
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+}
+
+public class StaffSummaryViewModel
+{
+	public class InstructorViewModel
+	{
+		public Guid InstructorSchoolYearGuid { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+
+	public string OrganizationName { get; set; }
+	public string Status { get; set; }
+	public List<InstructorViewModel> Instructors { get; set; }
+}
---
+++ Dal/Models/Views/Reporting/StudentSurvey.cs
@@ -0,0 +1,13 @@
+﻿namespace GrantTracker.Dal.Models.Views.Reporting;
+
+public class StudentSurveyViewModel
+{
+	public string OrganizationName { get; set; }
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+	public string MatricNumber { get; set; }
+	public string Grade { get; set; }
+	public string Activity { get; set; }
+	public int TotalDays { get; set; }
+	public double TotalHours { get; set; }
+}
